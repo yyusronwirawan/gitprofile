838da93dd0512247b44bad7d3f17c549135c7085
Yusron Wirawan <118438035+yyusronwirawan@users.noreply.github.com>
2023-08-09 23:28:00 +0700

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -119,7 +119,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'forest',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
